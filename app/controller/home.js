86aac4678b9c7890bb7379a3a04594c50d675d0c
令狐少侠 <czc12580520@gmail.com>
2019-09-02 00:54:23 +0800

    :horse:

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const cheerio = require('cheerio');
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    const res = await ctx.curl('http://class.sise.com.cn:7001/sise/', {
+      dataType: 'json',
+    });
+    const $ = cheerio.load(res.data);
+    console.log($('#token').text());
+
+    ctx.status = 200;
+    ctx.body = 'res.data';
   }
 }
 
